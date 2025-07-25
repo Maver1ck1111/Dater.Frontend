6029ec66124c7d15ef38fd54f7bfadd2c02be490
Maver1ck1111 <borisovd885@gmail.com>
2025-07-25 19:22:59 +0300

    changed styles for auth form and added styles for profile form

--- src/components/Auth/AuthForm.tsx
+++ src/components/Auth/AuthForm.tsx
@@ -18,28 +18,40 @@ export default function AuthForm({onSubmitFunction, enterText, buttonText, link}
     });
 
     return (
-        <form onSubmit={handleSubmit(onSubmitFunction)}>
+        <form onSubmit={handleSubmit(onSubmitFunction)} className="auth-form">
             <h1>{enterText}</h1>
 
             <div className={`form-field ${errors.email ? "has-error" : ""}`}>
                 <label htmlFor="email">Email</label>
-                <input {...register("email")} id="email" placeholder="Enter your email" />
+                <input
+                {...register("email")}
+                id="email"
+                placeholder="Enter your email"
+                type="email"
+                autoComplete="email"
+                />
                 {errors.email && <p className="error-message">{errors.email.message}</p>}
             </div>
 
-            <div className={`form-field ${errors.email ? "has-error" : ""}`}>
+            <div className={`form-field ${errors.password ? "has-error" : ""}`}>
                 <label htmlFor="password">Password</label>
-                <input {...register("password")} id="password" placeholder="Enter your password"/>
-                { errors.password && <p className="error-message">{errors.password.message}</p>}
+                <input
+                {...register("password")}
+                id="password"
+                placeholder="Enter your password"
+                type="password"
+                autoComplete="current-password"
+                />
+                {errors.password && <p className="error-message">{errors.password.message}</p>}
             </div>
 
             {link && (
-                <Link to="/login">
-                    Already have an account?
+                <Link to="/login" className="login-link">
+                Already have an account?
                 </Link>
             )}
 
-            <button>{buttonText}</button>
+            <button type="submit" className="submit-btn">{buttonText}</button>
         </form>
     )
 }
