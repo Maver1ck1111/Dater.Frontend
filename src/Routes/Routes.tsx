442d04ff9f1fe91aa2f93a6f15bad0baa6bfb3f6
Maver1ck1111 <borisovd885@gmail.com>
2025-08-23 12:54:27 +0300

    changed react router to new style using RouterProvider and createBrowserRouter

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -2,10 +2,11 @@ import App from "../components/layout/App";
 import Login from "../components/Auth/Login";
 import Register from "../components/Auth/Register";
 import ProfileForm from "../components/Profile/ProfileForm";
+import { createBrowserRouter } from "react-router-dom";
 
-export const routes = [
-    { path: "/", element: <App /> },
-    { path: "/login", element: <Login/>},
-    { path: "/register", element : <Register /> },
-    { path: "/profileform", element: <ProfileForm />}
-]
+export const routes = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+  { path: "/profileform", element: <ProfileForm /> },
+]);
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { routes } from './Routes/Routes.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./Routes/Routes.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {routes.map((route, index) => (
-          <Route key={index} path={route.path} element={route.element} />
-        ))}
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <RouterProvider router={routes}></RouterProvider>
+  </StrictMode>
+);
